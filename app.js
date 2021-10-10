ac69eff18cc5f64eb6f5b6454543ed227d3769a9
Michael Gill <mike@cloudgalacticos.co.uk>
2021-10-10 20:47:30 +0100

    Add CORS

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
+const cors = require('cors');
+app.use(cors());
 app.get('/hello', (req, res) => {
     console.log('Hello Express!');
     res.status(200).json({ message: 'Hello World' });
