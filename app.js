8da763bb25ab659e9de7aeb80b182cf8668344f2
Michael Gill <mike@cloudgalacticos.co.uk>
2021-10-11 11:57:18 +0100

    Updatefor demo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
-const cors = require('cors');
+const HOST = process.env.HOST || "localhost";
+const cors = require("cors");
 app.use(cors());
-app.get('/hello', (req, res) => {
-    console.log('Hello Express!');
-    res.status(200).json({ message: 'Hello World' });
+app.get("/hello", (req, res) => {
+    console.log("Hello Raghav!");
+    res.status(200).json({ message: "Hello Raghav!" });
 });
 app.listen(PORT, () =>
     console.log(`✅ API Server started: http://${HOST}:${PORT}`)
